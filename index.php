a1ec410ecfe3df3ccc049bd5343e6154f408b0c3
Mike <mikedejong4996@gmail.com>
2020-03-30 14:21:18 +0200

    Bestanden aangemaakt, start project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "model/logic.php";?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lobster"/>
+
+</head>
+<body>
+
+</body>
+</html>

be9fe93921ee505397143f2fdd08f2c22de15f7b
Mike <mikedejong4996@gmail.com>
2020-04-09 17:56:04 +0200

    Javascript verplaatst naar een folder

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ include "model/logic.php";
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script src="https://kit.fontawesome.com/2f6bc0b605.js" crossorigin="anonymous"></script>
-	<script src="javascript.js"></script>
+	<script src="js/javascript.js"></script>
 </head>
 	<body>
 		<div class="container">
